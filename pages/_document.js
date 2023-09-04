d233c7adc660d81dcbb5adad24efcb1792c1a4d9
khadijamess <khadijamess54@gmail.com>
2023-09-04 19:41:36 +0100

    parag

--- pages/_document.js
+++ pages/_document.js
@@ -23,13 +23,25 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
         </Head>
         <body>
           <Main />
           <NextScript />
+
+          <p>YouTube est un site web d'hébergement de vidéos et média social sur lequel les utilisateurs peuvent envoyer, rega
+            rder, commenter, évaluer et partager des vidéos en streaming. Il est créé en février 2005 par Steve Chen, Chad Hurley et Jawed
+             Karim, trois</p>
+             <p>YouTube est organisé en catégories thématiques avec un moteur de recherche qui permet de trouver 
+              un contenu via des mots-clés. Si l'essentiel des vidéos sont mises en lignes par des particuliers appelés « youtubeurs », l
+              e site a progressivement évolué en plateforme de divertissement généraliste et accueille de nombreuses chaînes officielles 
+              de médias traditionnels. Les chaînes YouTube les plus populaires et les partenaires officiels du site perçoivent un pourcen
+              tage des revenus publicitaires qu'ils contribuent à générer.</p>
+
+              
         </body>
+
       </Html>
     );
   }
